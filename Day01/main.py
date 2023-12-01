efc9115a7fb4d4f6ed4c018df98b442739956a36
William Owens <williamlewisowens@gmail.com>
2023-12-01 14:51:06 -0600

    Cleaned up a bit

--- Day01/main.py
+++ Day01/main.py
@@ -30,18 +30,6 @@ REV_TRANSLATION = {
    't': ['thgie'],
 }
 
-REV_NUM_TRANSLATION = {
-  'eno': 1,
-  'owt': 2,
-  'eerht': 3,
-  'ruof': 4,
-  'evif': 5,
-  'xis': 6,
-  'neves': 7,
-  'thgie': 8,
-  'enin': 9
-}
-
 def readFile(fileName):
     dir = os.path.abspath(os.path.dirname(__file__))
     with open(os.path.join(dir, fileName)) as file:
@@ -56,18 +44,17 @@ def helper(line, position, reversed):
         return NUM_TRANSLATION.get(line[position:(len(num) + position)])
   else:
     for num in REV_TRANSLATION.get(line[position]):
-      if ( REV_NUM_TRANSLATION.get(line[position:(len(num) + position)])):
-        return REV_NUM_TRANSLATION.get(line[position:(len(num) + position)])
+      if ( NUM_TRANSLATION.get(line[(position + 1 - len(num)):position + 1])):
+        return NUM_TRANSLATION.get(line[(position + 1 - len(num)):position + 1])
   return 0
 
 
 def solution(calibrationDocument, useWords):
   runningTotal = 0
   for line in calibrationDocument:
-    reversedLine = line[::-1]
     lineNumber = 0
     lookingLeft, lookingRight = True, True
-    l, rev = 0, 0
+    l, rev = 0, len(line) - 1
 
     while (lookingLeft):
       if (line[l].isnumeric()):
@@ -85,19 +72,19 @@ def solution(calibrationDocument, useWords):
         l += 1
     
     while (lookingRight):
-      if (reversedLine[rev].isnumeric()):
-        lineNumber += int(reversedLine[rev])
+      if (line[rev].isnumeric()):
+        lineNumber += int(line[rev])
         lookingRight = False
       # This elif block is skipped if only looking for numbers
-      elif (lookingRight and useWords and REV_TRANSLATION.get(reversedLine[rev])):
-        numToCheck = helper(reversedLine, rev, True)
+      elif (lookingRight and useWords and REV_TRANSLATION.get(line[rev])):
+        numToCheck = helper(line, rev, True)
         if (numToCheck):
           lineNumber += numToCheck
           lookingRight = False
         else:
-          rev += 1
+          rev -= 1
       else:
-        rev += 1
+        rev -= 1
 
     runningTotal += lineNumber
   return runningTotal
